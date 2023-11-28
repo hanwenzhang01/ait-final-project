00cb78dfe88c90fb1c145e32f58c59fa5f6ec8a9
hanwenzhang <hanwen.zhang.2011@gmail.com>
2023-11-27 21:18:12 -0500

    more tinkering w facile

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,30 +1,34 @@
 console.log('in main.js')
-import { Validator, enLang as en } from '@upjs/facile-validator';
+function main () {
+  import { Validator, enLang as en } from '@upjs/facile-validator';
 
-// Select the container element that contains the fields
-const form = document.querySelector('form');
+  // Select the container element that contains the fields
+  const form = document.querySelector('form');
 
-// Create an instance of Validator for the container element
-const v = new Validator(form, {
-  lang: en,
-});
+  // Create an instance of Validator for the container element
+  const v = new Validator(form, {
+    lang: en,
+  });
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  // Call validate method to start validation
-  console.log('before v.validate()')
-  v.validate();
-});
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    // Call validate method to start validation
+    console.log('before v.validate()')
+    v.validate();
+  });
 
 
-// Handle error-free validation
-v.on('validation:success', () => {
-  console.log('in validation success');
-  alert('Nice! The form was validated without any errors');
-});
+  // Handle error-free validation
+  v.on('validation:success', () => {
+    console.log('in validation success');
+    alert('Nice! The form was validated without any errors');
+  });
 
-// Handle failed validation
-v.on('validation:failed', () => {
-  console.log('in validation failure');
-  alert('Oops! There are some errors in the form.');
-});
+  // Handle failed validation
+  v.on('validation:failed', () => {
+    console.log('in validation failure');
+    alert('Oops! There are some errors in the form.');
+  });
+}
+
+document.addEventListener('DOMContentLoaded', main);
