bd58283299bc393517f5b040d3ce8a8603b386bb
hanwenzhang <hanwen.zhang.2011@gmail.com>
2023-12-01 00:40:16 -0500

    tinkering with SweetAlert

--- app.mjs
+++ app.mjs
@@ -82,7 +82,7 @@ function filterBooks(req) {
 
   app.get('/delete', async(req,res) => {
     //document.addEventListener("load", function(){
-      Swal.fire({
+      const swal = Swal.fire({
         title: "Are you sure you want to clear your shelf?",
         text: "You won't be able to revert this!",
         icon: "warning",
@@ -90,7 +90,9 @@ function filterBooks(req) {
         confirmButtonColor: "#3085d6",
         cancelButtonColor: "#d33",
         confirmButtonText: "Yes, delete it!"
-      }).then((result) => {
+      });
+      
+      swal.then((result) => {
         if (result.isConfirmed) {
           deleteAll();
           Swal.fire({
